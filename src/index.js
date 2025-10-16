9fca802080d6a2f36a1da74b1ef7a1759b847905
Venkman31 <jamie.wickett@a-v-e.com>
2025-10-16 16:35:53 +0100

    Initial project setup and code upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BudgetForecastingTool from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BudgetForecastingTool />
+  </React.StrictMode>
+);
